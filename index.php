07728315054f31b72e1301888ca44711c05e46b7
Aniket vishwakarma <76620615+Firestorm-23@users.noreply.github.com>
2023-05-03 00:11:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['userId'])){ header('location:login.php');}
+if(!isset($_SESSION['userId'])){ header('location:mindex.php');}
 ?>
 <!DOCTYPE html>
 <html>
