d8868113c7405e89063c4859bf82e7cb8367dd45
Aniket vishwakarma <76620615+Firestorm-23@users.noreply.github.com>
2023-05-03 01:00:18 +0530

    Update db.php

--- assets/db.php
+++ assets/db.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
     $con = new mysqli('pmcbankk.mysql.database.azure.com','rupesh2312','Bhimsen85%','mybank');
     define('bankName', 'PMC Bank',true);
     $ar = $con->query("select * from userAccounts,branch where id = '$_SESSION[userId]' AND userAccounts.branch = branch.branchId");
